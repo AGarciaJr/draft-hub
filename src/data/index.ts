0e5b73cb054f584d1347f99bc364191b1481ae9f
Alex <alejandrogarciacsi@gmail.com>
2025-05-21 00:51:12 -0500

    Finally fixing merge? Also Lint and Build?

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,5 @@
-import internData from './intern_project_data.json' assert { type: 'json' };
-import type { PlayerBio } from '../types/player.types';
+import internData from './intern_project_data.json';
+import type { PlayerBio } from '../types/player.types.ts';
 
 export const playerBios: PlayerBio[] = internData.bio;
 export const scoutRankings = internData.scoutRankings; 
