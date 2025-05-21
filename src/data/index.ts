3f8d04a2b2118d050d0c8dcc3ae006269ca18d31
Alex <alejandrogarciacsi@gmail.com>
2025-05-21 00:56:06 -0500

    Successful production build

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,4 +2,4 @@ import internData from './intern_project_data.json';
 import type { PlayerBio } from '../types/player.types.ts';
 
 export const playerBios: PlayerBio[] = internData.bio;
-export const scoutRankings = internData.scoutRankings; 
+export const scoutRankings = internData.scoutRankings;
