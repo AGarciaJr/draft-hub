04135b7f1457ae8bd21fa534292551cccfd49d9f
AGarciaJr <alejandrogarciacsi@gmail.com>
2025-05-23 22:23:21 -0500

    refactor: make position field optional in PlayerBio type to match actual data structure

--- src/types/player.types.ts
+++ src/types/player.types.ts
@@ -16,6 +16,7 @@ export interface PlayerBio {
   currentTeam: string;
   league: string;
   leagueType: string;
+  position?: string;
 }
 
 export interface ScoutRanking {
@@ -103,7 +104,7 @@ export interface SeasonLog {
   "FG2%": number;
   FGM3: number;
   FGA3: number;
-  "FG3%": number;
+  "3P%": number;
   FTM: number;
   FTA: number;
   "FT%": number;
@@ -126,8 +127,6 @@ export interface ScoutingReport {
   report: string;
   date?: string;
 }
-
-
 
 export interface PlayerStats {
   totalPlayers: number;
